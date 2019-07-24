85d6df4812e743894b083709935f2ed3c2bc7930
Mark Kampstra <mark.kampstra@gmail.com>
2019-07-24 20:52:56 +0200

    Change zoom level

--- map2.php
+++ map2.php
@@ -32,7 +32,7 @@
 
       function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 6,
+          zoom: 4,
           center: {lat: 59.334591, lng: 18.063240},
           mapTypeId: 'terrain'
         });
